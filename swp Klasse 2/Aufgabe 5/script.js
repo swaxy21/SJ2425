4a7e303e11e5dfd591bb7dec631ab28ee29aa34f
swaxy21 <144341356+swaxy21@users.noreply.github.com>
2024-10-18 14:59:59 +0200

    2.Klasse SWP

--- swp Klasse 2/Aufgabe 5/script.js
+++ swp Klasse 2/Aufgabe 5/script.js
@@ -7,8 +7,7 @@ function hinzufügen() {
     const produktName = selectedOption.getAttribute("data-name");
     const preis = parseFloat(selectedOption.value);
 
-    if (preis > 0) {  // Prüfen, ob eine gültige Option ausgewählt wurde
-        // Neue Zeile zur Tabelle hinzufügen
+    if (preis > 0) { 
         const table = document.getElementById("warenkorb");
         const newRow = table.insertRow();
         const cell1 = newRow.insertCell(0);
@@ -17,10 +16,11 @@ function hinzufügen() {
         cell1.innerHTML = produktName;
         cell2.innerHTML = preis.toFixed(2) + "€";
 
-        // Gesamtsumme aktualisieren
+
         gesamtSumme += preis;
         document.getElementById("gesm").innerHTML = `<p>Gesamtsumme: ${gesamtSumme.toFixed(2)}€</p>`;
     }
+
 }
 
 
